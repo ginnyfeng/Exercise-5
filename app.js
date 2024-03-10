11262e4af5f14ff61b641044bba322fae5d766bd
Ginny Feng <ginnyfengdog@gmail.com>
2024-03-10 15:27:13 -0500

    Exercise 5

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
-function validateForm() {
-	let a = document.forms["Form"]["firstname"].value;
-	if (a == "") {
-	  alert("First name must be filled out");
-	  return false;
+function formValidator(form1, form2, form3, form4){
+  
+	function form1(fname){
+	  if ( fname.length <= 0) {
+	console.log("First name is missing");
 	}
-
-	let b = document.forms["Form"]["lastname"].value;
-	if (b == "") {
-	  alert("Last name must be filled out");
-	  return false;
+	  if(typeof(fname) !== 'string'){
+		console.log("First name must be a string");
+	  }else{
+		console.log("");
+	  }
 	}
-
-	let c = document.forms["Form"]["age"].value;
-	if (c == "") {
-	  alert("Age must be filled out");
-	  return false;
+	form1("Ginny");
+	//============================
+	
+	function form2(lname){
+	  if (lname.length <= 0) {
+	console.log("Last name is missing");
 	}
-
-	let d = document.forms["Form"]["phone"].value;
-	if (d == "") {
-	  alert("Phone # must be filled out");
-	  return false;
+	  if(typeof(lname) !== 'string'){
+		console.log("Last name must be a string");
+	  }else{
+		console.log("");
+	  }
 	}
-  }
+	form2("Feng");
+	//==============================
+	
+	function form3(age){
+	  if (age.length <= 0) {
+	console.log("Age is missing");
+	}
+	  if (typeof(age)==='string'||typeof(age)==='boolean'){
+		console.log("Age must be a number");
+	  }
+	  if(age<18){
+		console.log("Sorry, not old enough for our app");
+	  }
+	  else{
+		console.log("");
+	  }
+	}
+	form3(17);
+	//==============================
+	
+	function form4(phone){
+	  if ( phone.length <= 0) {
+	console.log("Phone is missing");
+	}
+	  if(typeof(phone) !== 'String'){
+		console.log("Phone must be a string");
+	  }else{
+		console.log("");
+	  }
+	}  
+	  form4("9017991161");
+	}
+	
+	
+	
